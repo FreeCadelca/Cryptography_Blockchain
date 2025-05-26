22bb410c4f1233b0a15067e4592d091554a9ac7b
Dmitriy <746503645272548akk@gmail.com>
2025-05-27 02:36:21 +0300

    LSP functions

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pprint
+import numpy
+import numpy as np
 
-pi = (252, 238, 221, 17, 207, 110, 49, 22, 251, 196, 250, 218, 35, 197, 4, 77, 233, 119, 240, 219, 147,
+PI = (252, 238, 221, 17, 207, 110, 49, 22, 251, 196, 250, 218, 35, 197, 4, 77, 233, 119, 240, 219, 147,
       46, 153, 186, 23, 54, 241, 187, 20, 205, 95, 193, 249, 24, 101, 90, 226, 92, 239, 33, 129, 28, 60, 66, 139, 1,
       142, 79, 5, 132, 2, 174, 227, 106, 143, 160, 6, 11, 237, 152, 127, 212, 211, 31, 235, 52, 44, 81, 234, 200, 72,
       171, 242, 42, 104, 162, 253, 58, 206, 204, 181, 112, 14, 86, 8, 12, 118, 18, 191, 114, 19, 71, 156, 183, 93, 135,
@@ -12,22 +14,50 @@ pi = (252, 238, 221, 17, 207, 110, 49, 22, 251, 196, 250, 218, 35, 197, 4, 77, 2
       170, 144, 202, 216, 133, 97, 32, 113, 103, 164, 45, 43, 9, 91, 203, 155, 37, 208, 190, 229, 108, 82, 89, 166,
       116, 210, 230, 244, 180, 192, 209, 102, 175, 194, 57, 75, 99, 182)
 
-IV = '00000001' * 64
+IV = [int(i) for i in '00000001' * 64]
 
-teta = (0, 8, 16, 24, 32, 40, 48, 56, 1, 9, 17, 25, 33, 41, 49, 57, 2, 10, 18, 26, 34, 42, 50, 58, 3, 11, 19, 27,
+TETA = (0, 8, 16, 24, 32, 40, 48, 56, 1, 9, 17, 25, 33, 41, 49, 57, 2, 10, 18, 26, 34, 42, 50, 58, 3, 11, 19, 27,
         35, 43, 51, 59, 4, 12, 20, 28, 36, 44, 52, 60, 5, 13, 21, 29, 37, 45, 53, 61, 6, 14, 22, 30, 38, 46, 54, 62, 7,
         15, 23, 31, 39, 47, 55, 63)
 
 a_matrix_compressed = []
 with open("ssss") as f:
-      a_matrix_compressed = [i.rstrip() for i in f.readlines()]
+    a_matrix_compressed = [i.rstrip() for i in f.readlines()]
 
-a_matrix = []
+A_MATRIX = []
 for i in a_matrix_compressed:
-      new_line = []
-      for j in i:
-            j_to_bits = [int(bit) for bit in format(int(j, 16), '04b')]
-            new_line += j_to_bits
-      a_matrix.append(new_line)
+    new_line = []
+    for j in i:
+        j_to_bits = [int(bit) for bit in format(int(j, 16), '04b')]
+        new_line += j_to_bits
+    A_MATRIX.append(new_line)
+
+
 # for i in a_matrix:
 #       print(*i)
+
+def L(v: list[int]) -> list[int]:
+    a = [v[i * 64:i * 64 + 64] for i in range(8)]
+    res = []
+    for i in range(len(a)):  # mb reverse
+        res += [int(k) for k in np.array(a[i]) @ np.array(A_MATRIX)]
+    return res
+
+
+def S(v: list[int]) -> list[int]:
+    a = [int(''.join(map(str, v[i * 8:i * 8 + 8])), 2) for i in range(64)]
+    res = []
+    for i in range(len(a)):  # mb reverse
+        new_value_bin = bin(PI[a[i]])[2:]
+        res += [int(i) for i in new_value_bin]
+    return res
+
+def P(v: list[int]) -> list[int]:
+    a = [int(''.join(map(str, v[i * 8:i * 8 + 8])), 2) for i in range(64)]
+    res = []
+    for i in range(len(a)):  # mb reverse
+        new_value_bin = bin(a[TETA[len(a) - i - 1]])[2:]
+        res += [int(i) for i in new_value_bin]
+    return res
+
+
